82ec3fd65f421180993cbba6628ba79e2e006aa2
Liana-Balayan <liana.balayan.83@mail.ru>
2024-09-17 19:10:29 +0300

    Add files via upload

--- Homework.js
+++ Homework.js
@@ -1,1 +1,61 @@
-const a = 1
+// let a = +prompt();
+// if(a % 2 === 0){
+//     console.log("Even");
+    
+// }else{
+//     console.log("Odd");
+    
+// }
+
+// 2.Given two numbers print 1 if one of them is 
+// divisible by the other one, otherwise print 0.
+
+// let a = +prompt();
+// let b = +prompt();
+// if(a % b === 0 || b % a === 0){
+//     console.log(1);
+    
+// }else{
+//     console.log(0);
+    
+// }
+
+// 3. Given two variables, which are the angles of a
+//  triangle. Find the third angle of the
+// triangle. (Sum of the angles of a triangle equals 
+//     180 degrees).
+// let angl1 = +prompt();
+// let angl2 = +prompt();
+// let angl3 = 180 - (angl1 + angl2);
+// console.log(angl3);
+
+// 4. Given five numbers as input. Calculate and 
+// print the average of the numbers(without
+//     using arrays).
+// let a = +prompt();
+// let b = +prompt();
+// let c = +prompt();
+// let d = +prompt();
+// let e = +prompt();
+// let average = (a + b + c + d + e) / 5;
+// console.log(average);
+
+// 5. Check if a number is a multiple of 3, 5 or 7 
+// and output the appropriate message.
+// let a = +prompt();
+// if(a % 3 === 0 && a % 7 === 0){
+//     console.log("a is a multiple of 3 and 7");
+    
+// }else if(a % 5 === 0 && a % 7 === 0){
+//     console.log("a is a multiple of 5 and 7");
+    
+// }else if(a % 3 !== 0 && a % 5 !== 0 && a % 7 !== 0){
+//     console.log("13 is not a multiple of 3, 5 or 7");
+
+// }else if(a % 3 === 0 && a % 5 === 0 && a % 7 === 0){
+//     console.log("a is a multiple of 3, 5 and 7");
+    
+// }else if(a % 3 === 0){
+//     console.log("a is a multiple of 3");
+    
+// }
