8e71478dc86253d8a1b846968dd704795e1ef6ad
Liana-Balayan <liana.balayan.83@mail.ru>
2024-09-17 20:29:46 +0300

    Add files via upload

--- Homework.js
+++ Homework.js
@@ -42,20 +42,21 @@
 
 // 5. Check if a number is a multiple of 3, 5 or 7 
 // and output the appropriate message.
-// let a = +prompt();
-// if(a % 3 === 0 && a % 7 === 0){
-//     console.log("a is a multiple of 3 and 7");
-    
-// }else if(a % 5 === 0 && a % 7 === 0){
-//     console.log("a is a multiple of 5 and 7");
-    
-// }else if(a % 3 !== 0 && a % 5 !== 0 && a % 7 !== 0){
-//     console.log("13 is not a multiple of 3, 5 or 7");
 
-// }else if(a % 3 === 0 && a % 5 === 0 && a % 7 === 0){
-//     console.log("a is a multiple of 3, 5 and 7");
-    
-// }else if(a % 3 === 0){
-//     console.log("a is a multiple of 3");
-    
+// let a = +prompt("Enter a number");
+
+// if (a % 3 === 0 && a % 5 === 0 && a % 7 === 0) {
+//     console.log(`${a} is a multiple of 3, 5 and 7.`);
+// } else if (a % 3 === 0 && a % 7 === 0) {
+//     console.log(`${a} is a multiple of 3 and 7.`);
+// } else if (a % 5 === 0 && a % 7 === 0) {
+//     console.log(`${a} is a multiple of 5 and 7.`);
+// } else if (a % 3 === 0) {
+//     console.log(`${a} is a multiple of 3.`);
+// } else if (a % 5 === 0) {
+//     console.log(`${a} is a multiple of 5.`);
+// } else if (a % 7 === 0) {
+//     console.log(`${a} is a multiple of 7.`);
+// } else {
+//     console.log(`${a} is not a multiple of 3, 5 or 7.`);
 // }
