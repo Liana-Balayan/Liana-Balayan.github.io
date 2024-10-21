a20d7f0b06cc477920333587c39ef2c13a1865cb
Liana-Balayan <liana.balayan.83@mail.ru>
2024-10-21 21:02:12 +0300

    Add files via upload

--- Homework.js
+++ Homework.js
@@ -1,66 +1,45 @@
 ////// տ ն ա յ ի ն ա շ խ ա տ ա ն ք /////////
  
 
-//1. Given a number. Print the sum of digits.
-////// while  //////
-let num = +prompt();
-let sum = 0;
-while(num > 0){
-  sum +=num % 10;
-  num = Math.floor(num / 10)
+function longestWord(sentence) {
+    let longest = '';
+    let Word = '';
+    for (let i = 0; i < sentence.length; i++) {
+        if (sentence[i] === ' ' || i === sentence.length - 1) {
+            if (i === sentence.length - 1) {
+                Word += sentence[i]; 
+            }
+            if (Word.length >= longest.length) {
+                longest = Word;
+            }
+            Word = '';
+        } else {
+            Word += sentence[i];
+        }
+    }    
+    return longest;
 }
-console.log(sum);
+console.log(longestWord("A revolution without dancing is a revolution not worth having")); 
 
-///// for  ///////
-let i = +prompt();
-let sum = 0;
-for(; i > 0; i = Math.floor(i / 10) ){
-  sum += i % 10;
+
+
+//3.Write a function to compute a new string from the given one by
+// moving the first char to
+//come after the next two chars, so &quot;abc&quot; yields &quot;
+//bca&quot;. Repeat this process for each
+//subsequent group of 3 chars. Ignore any group of fewer than 3 chars
+// at the end.
+ 
+function fn(str){
+     let res = "";
+for(let i = 0; i < str.length; i = i + 3){
+     if (i + 2 < str.length){
+          let first = str[i];
+          let second = str[i + 1];
+          let third = str[i + 2];
+          res += third + second + first 
+     }
 }
-console.log(sum);
-
-//2. Given a number. Print the multiplication of 
-//digits.
-////// while  //////
-let num = +prompt();
-let sum = 1;
-while(num > 0){
-  sum *=num % 10;
-  num = Math.floor(num / 10)
+        return res
 }
-console.log(sum);
-
-///// for   //////
-let i = +prompt();
-let sum = 1;
-for(; i > 0; i = Math.floor(i / 10) ){
-  sum *= i % 10;
-}
-console.log(sum);
-
-
-//3. Insert a number. Calculate product and sum of 
-//the digits of the number. If product is
-//divisible by the sum, print the quotient, otherwise
-// print the remainder.
-
-
-let num = +prompt()
-let sum = 0;
-let product = 1;
-for( ; num > 0;num =Math.floor(num / 10)){
-  for( ; num > 0;num =Math.floor(num / 10)){
-  sum +=num % 10;
-  product *=num % 10;
-  };
-  console.log(sum);  
-  }
-  console.log(product);
-
-if(product / sum === 0){
-  console.log(product / sum);
-  
-}else{
-  console.log(product % sum);
-  
-}
+console.log(fn("abcdefgh"));
