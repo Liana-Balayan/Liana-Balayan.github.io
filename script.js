e25b8c464263ebfb099d657c2cfc7c0e1d807e0d
Liana-Balayan <liana.balayan.83@mail.ru>
2025-02-04 15:48:02 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,832 +1,98 @@
-// let  a = +prompt("number")
-// let  b = +prompt("number")
-// let  x = a + b;
-// let  y = a - b;
-// let  z = a * b;
-// let xy = a / b;
-// console.log(x + " " + y + " " + z + " " + xy);
-
-// let a = +prompt("number");
-// let b = +prompt("number");
-// let c = +prompt("number");
-// let p = a + b + c;
-// let p0 = p / 2
-// let s = ((p0 *(p0- a) * (p0 - b) * (p0 - c)) ** 0.5)
-// console.log(p)
-// console.log(s)
-
-// let tc = (20);
-// let tf =tc * 9/5 + 32;
-// let tk = tc + 273; 
-// console.log(tf);
-// console.log(tk);
-
-// let t1 = (1983);
-// let t = 365 * 24 * 60;
-// let t2 =  (2024);
-// let t3 = (t2 - t1) * t;
-// console.log(t3);
-
-// let a = ("Hello");
-// let b = ("Liana");
-// let c =   a + " "+  b  ;
-// console.log(c);
-
-// let a = 10;
-// let b = 20;
-// let c = a - b;
-// let d = (a - b) * a * b;
-// a = 30; 
-// console.log(d % a);
-
-// let a = 2;
-// a = 2 ** 10;
-// let b = a **(1/3);
-// console.log(b);
-
-//  let name = prompt("what is your name?");
-//  let age = prompt("How old are you?");
-//  alert(name + age ); 
-
-// let age = prompt("How old are you?");
-// console.log(age * 365);
-
-// let name = prompt("name");
-// alert("hello" + " " + name);
-
-// 7. +prompt(a = 2);
-//    +prompt(b = 4);
-//    +prompt(c = 8);
-// alert (p = a + b +c);
-// alert (p0 = p / 2);
-
-// let a = +prompt();
-// let b = +prompt();
-// let p = (a + b) * 2;
-// let s = a * b;
-// console.log(p);
-// console.log(s);
-
-// let r = +prompt();
-// let pi= +prompt();
-// let s = p * (r**2);
-// let l = 2 * p * r;
-// console.log(s);
-// console.log(l);
-
-// let v = +prompt();
-// let t = +prompt();
-// let s = v * t;
-// console.log(s);
-
-//  let name = prompt("what is your name");
-//    alert("Hello " + " " +  name );
-// let name = prompt("what is your name?");
-// let a = 365; 
-// console.log( name + " " + 40 * a);
 
 
-// let name = prompt ("what is your name?");
-// let age = +prompt( "how old are you?");
-// alert("my name is " + " "+  name + " I am "+ " "  +  age );
-
-//  +prompt(a = 2);
-//  +prompt(b = 5);
-//  +prompt(c = 8);
-//  alert(a + b + c);
-//  alert( a - b - c);
-//  alert( a * b * c);
-//  alert(a / b / c );
-
-//  let a = +prompt();
-//  let b = +prompt();
-//  let c = +prompt();
-//  console.log(p = (a +  b + c)/2);
-//  console.log (s = ((p * (p - a )* p * (p - b )* p *(p - c)))**0.5);
-
-//  let a  = +prompt("nermucenk a - n");
-//  let b =  +prompt("nermucenk b - n");
-//  console.log(a > b);
-//  console.log(a < b);
-//  console.log(a === b);
-//  console.log(a !== b);
-//  console.log(a  == b);
-//  console.log(a >= b);
-//  console.log(a <= b);
-
-// let a = +prompt();
-// let b = +prompt();
-// let p = (a + b) * 2;
-// let s = a * b;
-// console.log(p);
-// console.log(s);
-
-// number  -((2**53)-1)    ((2**53)-1) 
-//string 
-//boolean 
-
-// let x = prompt();
-// console.log(Number(x) + 10);
-
-//let x = +prompt();
-//let x = true;
-// console.log(typeof x);
-
-// console.log(+"42");
-// console.log(+true);
-// console.log(+false);
-// console.log(+"text");
-// console.log(NaN);
-// console.log(typeof NaN);
-
-
-// let x = 0;
-// let y = Boolean(x);
-// console.log(y);
-
-// console.log(!!true);
-
-// 0 === -0 === +0;
-
-
-// let number = +prompt("")
-// if(number < 0 ) { 
-//     alert("bacasakan");
-// }
-// else {
-//     alert("drakan")
-// }
-
-//  let x = +prompt("");
-//  let y = +prompt("");
-//  if (x % y == 0){
-//     alert("ayo")
-//  }
-// else {
-//     alert("voch")
-// }
-
-// let x = +prompt("");
-// let y = +prompt("");
-// let z = +prompt("");
-// if ( a + b > c){
-//    console.log(" erankyuni goyutyun uni");
-// }
-// if
-
-
-// let number = +prompt("");
-//  if (number < 0) {
-// console.log("bacasakan");
-//  }
-//   else { 
-//   console.log("drakan");
-//  }
-
-// let x = +prompt("");
-// let y = +prompt("");
-// if ( x % y == 0){
-//  console.log("mnacord chka");
-// }
-// if( x % y !== 0){
-//     console.log("mnacord ka");
-// }
-
-// let d = +prompt("");
-// let b = +prompt("");
-// let c = +prompt("");
-// if( d + b > c){
-//     console.log("erankyuni goyutyun uni");
-// }
-// if( d + b < c){
-//     console.log("erankyuni goyutyun chuni");
-// }
-
-// let x = + prompt("")
-// if (x % 2 == 0 ){
-//     console.log("zuyg e ");
-// }
-// if (x % 2 == 1 ){
-//     console.log("zuyg che");
-// }
-
-// let x = +prompt("")
-// if (x % 2 == 1){
-//     console.log("kent e");
-// }
-// if (x % 2 == 0){
-//     console.log("kent che");
-// }
-
-// let x = +prompt("grel tiv")
-// if (x % 2 == 0) {
-//     alert("kent che")
-// }
-// else {
-//     alert("kente")
-// }
-
-// let a = +prompt("tiv");
-// let b = +prompt("tiv");
-// let c = +prompt("tiv");
-// if( a == Number(a) && Number(b) && Number(c))
-//   if ( a > b & a > c){
-//     console.log("a ");
-// }else if(b > a && b > c){
-//     console.log(b);
-// }else if (c > b && c > a ){
-//     console.log("c");
-// }else if (a == b && b == c){
-//     console.log("bolor tvery havasar en");
-// } else if(b == c && a > b ){
-//     console.log(a);
-// }else if(a == c && b > a){
-//     console.log(b)
-// }else if(a == c && a > b){
-//     console.log(c);
-// }else if(b == c && b > a){
-//     console.log(b);
-// }else if(a == c && a > c){
-//     console.log(a);
-// }else{
-//     console.log("tveric meky number che");
-// }
-
-// 8.
-//  let a = prompt("were are your from?")
-// if(a =="Հայաստան"){
-//     alert("Ես հայ եմ")
-// }
-// if(a =="Ռուսաստան"){
-//     alert("Ես ռուս եմ")
-// }
-// if (a=="Ա Մ Ն"){
-//     alert("Ես ամերիկացի եմ")
-// }
-
-// /9.
-// let a = prompt(" ");
-// if(a =="գեղեցիկ"){
-//  console.log("beutiful");
-// }
-// if(a =="հոգատար"){
-//     console.log("careful");
-// }
-// if(a == "հարուստ"){
-//     console.log("rich");
-// }
-
-// 10.
-// let a = +prompt();
-// if (a == 1){
-//     alert("garun")
-// }
-// if (a == 2){
-//     alert("amar")
-// }
-// if (a == 3){
-//     alert("ashun")
-// }
-// if (a == 4){
-//     alert("dzmer")
-// }
-
-// 11.
-// let day = +prompt()
-// if(day === Number(day)){
-//     if (day > 10 & day <= 20){
-//         console.log("erkrord");
+//2//Given an array of nested arrays. Write a recursive 
+//function that flattens it. (Hint create function that 
+//concats arrays).
+// function flattenArray(arr) {
+//     // եթե զանգվածը դատարկ է, վերադարձնում ենք դատարկ զանգված
+//     if (arr.length === 0) {
+//         return [];
 //     }
-//     if( day > 20 & day <= 31){
-//         console.log("errord");
+
+//     // `arr[0]` (առաջին տարրը) կարող է լինել զանգված, թե ոչ
+//     if (Array.isArray(arr[0])) {
+//         // Եթե առաջին տարրը զանգված է, միացնում ենք այն հարթավորված մասի հետ
+//         return flattenArray(arr[0]).concat(flattenArray(arr.slice(1)));
+//     } else {
+//         // Եթե առաջին տարրը չի է զանգված, պարզապես ավելացնում ենք այն արդյունքի զանգվածին
+//         return [arr[0]].concat(flattenArray(arr.slice(1)));
 //     }
 // }
-// if (  day > 0 & 10 >= day ){
-//     console.log("arachin")
-// }else
-// {console.log("chka")
-// }
+
+// // Օրինակներ
+// console.log(flattenArray([1, [3, 4, [1, 2]], 10]))  
+// console.log(flattenArray([14, [1, [[[3, []]], 1], 0]]))  
 
 
-// 12.
-// let  month = +prompt();
-// if (month >= 1 & month <= 3){
-//     console.log("ձմեռ");
+//3//Given a number. Write a function that calculates its sum of
+//the digits and if that sum has more than 1 digit find the sum of
+//digits of that number. Repeat that process if needed and return
+//the result.
+// function sumDigits(num) {
+//     // Կոնվերտում ենք թիվը տող, որպեսզի կարելի լինի վերցնել յուրաքանչյուր թվանշան
+//     num = Math.abs(num);  
+//     let sum = 0;
+
+//     //  թվանշանների գումարը
+//     while (num > 0) {
+//         sum += num % 10;  // Ավելացնում ենք վերջին թվանշանը
+//         num = Math.floor(num / 10);  // Հանում ենք վերջին թվանշանը
 //     }
-// if(month >= 4 & month <=6){
-//     console.log("գարուն");
-// }
-// if (month >= 7 & month <= 9){
-//     console.log("ամառ");
-// }
-// if (month >= 10 & month <= 12){
-//     console.log("աշուն");
-// }
 
-// 14.
-// let a = +prompt();
-// let b = +prompt();
-// let c = a**b;
-// console.log(c);
-
-// 15
-// let a = +prompt();
-// if(a >= 1 & a <= 100){
-//     console.log("True");
-// }
-// else{
-//     console.log("False");
-// }
-
-// 16.
-// let a = +prompt();
-// if(a >= 1 & a <= 20){
-//     console.log("arajin");
-// }
-// if(a >= 21 & a <= 48){
-//     console.log("erkrord");
-// }
-// if(a >=49 & a <= 90){
-//     console.log("errord");
-// }
-
-// let a = +prompt()
-// let b = +prompt()
-// if( a % b == 0){
-//     console.log("ayo");
-// }
-// else{
-//     console.log("voch");
-// }
-
-// 1.
-// let a = +prompt();
-// if (a == 0){
-//     console.log("True");
-// }
-// else{
-//     console.log("False");
-// }
-
-// 2.
-// let a = +prompt();
-// if (a > 0){
-//     console.log("True");
-// }
-// else{
-//     console.log("False");
-// }
-
-// 3.
-// let a = +prompt();
-// if( a < 0){
-//     console.log("True");
-// }
-// else{
-//     console.log("False");
-// }
-
-// 4.
-// let a = +prompt();
-// if ( a >= 0){
-//     alert("True");
-// }
-// else{
-//     alert("False");
-// }
-
-// 5.
-// let a = prompt();
-// if( a <= 0){
-//     console.log("True");
-// }
-// else{
-//     console.log("False");
-// }
-
-// 6.
-// let a = +prompt();
-// if (a != 0){
-//     console.log("True");
-// }
-// else{
-//     console.log("False");
-// }
-
-// 7.
-// let a = prompt("");
-// if (a =='test'){
-//     console.log("True");
-// }
-// else{
-//     console.log("False");
-// }
-
-// 8.
-// let a = +prompt();
-// if(a ===1){
-//     console.log("True");
-// }
-// else{
-//     console.log("False");
-// }
-
-// 9.
-// let a = +prompt();
-// if(a > 0 & a < 5){
-//     console.log("True");
-// }
-// else{
-//     console.log("False");
-// }
-
-// 10.
-// let a = +prompt();
-// if(a == 0 | a == 2){
-//     console.log(a + 7);
-// }
-// else{
-//     console.log(a + 10);
-// }
-
-// 11.
-// let a = +prompt();
-// let b = +prompt();
-// if (a <=1 & b >= 3){
-//     console.log(a + b);
-// }
-// else{
-//     console.log(a - b);
-// }
-
-// 12.
-// let a = +prompt();
-// let b = +prompt();
-// if(a >2 & a < 11| b >= 6 & b < 14){
-//     console.log("True");
-// }
-// else{
-//     console.log("False");
-// }
-
-// let a = +prompt(1);
-// alert(1 - a)
-
-// let a = +prompt();
-// let b = +prompt();
-// let c = +prompt();
-// let D =b **2 - 4 * a * c;
-// let x1 = (-b + D **(1/2)) / (2 * a);
-// let x2 = (-b - D **(1/2)) / (2 * a);
-// console.log(D)
-// console.log(x1);
-// console.log(x2);
-
-//  let a = +prompt("a");
-//  let b = +prompt("b");
-//  let c = +prompt("c");
-//  if(a == Namber(a) && b == Number(b) && c == Number(c)){
-//     if( a == 0 && b == 0 && c == 0){
-//         console.log("R");
-//     }else if (a == 0 && b == 0 && c !== 0){
-//         console.log("No solution");
-//     }else if (a == 0 && c == 0){
-//         console.log("0");
-//     }else if (a === 0){
-//         let x = ((-1) * c) / b;
-//         console.log(x);
-//     }else if ( b == 0 && a > 0 && c > 0){
-//         console.log("T his equation has not real solutions");
-//     }else if ( b == 0 && a < 0 && c < 0){
-//         console.log(" This eqation has not real solutions");
-//     }else if ( b == 0 && a > 0 && c < 0){
-//         let x1 = (( c) / a) **(1/2);
-//         let x2 = (-1) * ((c) / a) **(1/2);
-//         console.log( x1 , x2);
-//     }else if (b ==0 && a < 0 && c > 0){
-//         let x1 =(( c ) / a)**(1/2);
-//         let x2 =(-1)* (( c)/a)**(1/2);
-//         console.log(x1, x2);
-//     }else if ( c == 0 && a !== 0 && b!== 0){
-//         let x = -b/ a;
-//         console.log(x , "0");
-//     }else{
-//         let d = (b**2)-(4 * a * c);
-//         if(d < 0){
-//             console.log("This eqaauation has not real solutions");
-//     }else if (d == 0);
-//         let x = -b /(2 * a);
-//         console.log(x);
-//     }else if(d > 0){
-//         let x1 = (-b + d ** (1/2))/( 2 * a);
-//         let x2 = (-b - d **(1/2)) /(2 *a);
-//         console.log(x1, " ",x2);
-//      }
+//     // Եթե ստացված գումարը մեկ թվանշան է, վերադարձնում ենք այն
+//     if (sum < 10) {
+//         return sum;
 //     }
-// }else{
-//     console.log(" one of them is not number");
+//     return sumDigits(sum);
 // }
-    
+// console.log(sumDigits(14));  
+// console.log(sumDigits(29));   
+// console.log(sumDigits(999999999999999));     
 
-
-// let x = 600;
-// while( x >= 200){
-//     console.log(x);
-//     x--
-// }
-
-// let x = 1;
-// while(x <= 1000){
-//     if( x % 2 == 0)  
-//  console.log(x);
-// x++
-// }
- 
-
-// let x = 1;
-// let sum = 1;
-// while (x <= 50) {
-//     if ( x % 2 == 1)
-//         console.log(x);
-//     i++ 
-// }
-
-
-// let x = 1;
-// while (x <= 100) {
-    
-//     console.log(x);
-//     x = x + 2;
-// }
-
-// let x = +prompt();
-// let y = +prompt();
-// let a;
-// if( x > y){
-//     a = y
-// }
-// console.log(a);
-
-// let x = 4;
-// let y = 5;
-// let c = x > 2 ? x + 1 : y > 2 ? y + 1 : 0;
-// console.log(c);
-
-// let x = +prompt();
-// let y = +prompt();
-// let a = +prompt();
-// let max = x > y ? x > a: x 
-
-// let x = 10;
-// let y = ++x
-// console.log(x);
-// console.log(y);
-
-// let x = 4;
-// let y = x++;
-// console.log(x);
-// console.log(y);
-
-// let n = +prompt();
-// let x = 0;
-// while (x <= n)
-//     if(n % x == 0){
-
-
+//4.// Given an array and a number N.  Write a recursive
+//function that rotates an array N places to the left. (Hint: to
+//add element to the beginning use arr.unshift())
+// function rotateArrayLeft(arr, N) {
+//     // Եթե N-ը 0 է կամ զանգվածը դատարկ է, վերադարձնում ենք այն առանց փոփոխությունների
+//     if (N === 0 || arr.length === 0) {
+//         return arr;
 //     }
-//     alert(x)
-
-
-//  let a = 0;
-// for( let i = 1; i <= 400; i++){
-//     if(i % 9 == 0){
-// a = a + i;
+//     if (N < 0) {
+//         return rotateArrayLeft(arr, arr.length + N);  
 //     }
+
+//     // Պտտում ենք առաջին տարրը դեպի վերջ և կիրառում ենք ռեկուրսիա մնացած տարրերի վրա
+//     arr.push(arr.shift());  // Զանգվածի առաջին տարրը տեղափոխում ենք վերջ
+//     return rotateArrayLeft(arr, N - 1);  // Կրկնում ենք N-1 անգամ
 // }
-// console.log(a);
+// console.log(rotateArrayLeft(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], 3));
+//  console.log(rotateArrayLeft(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], -2)); 
 
 
-// let a = 0;
-// let b = 0;
-// for(let  i= 1; i <= 100; i++){
-//     if(i % 2 == 0){
-//         a = a + i;
-//         }else{
-//             b = b + i
+//5.// Given an object. Invert it (keys become values and values
+  //  become keys). If there is more than key for that given value
+  //create an array.
+// function reverseObject(obj) {
+//     const reversed = {};  // Ստեղծում ենք նոր օբյեկտ
+
+//     // Անցնում ենք ամբողջ օբյեկտի վրայով
+//     for (let key in obj) {
+//         let value = obj[key];
+        
+//         // Եթե արժեքը արդեն կա reversed օբյեկտում
+//         if (reversed[value]) {
+//             // Ավելացնում ենք նոր բանալի, եթե արդեն արժեքի համար կա բազմակողմանի տարր
+//             reversed[value].push(key);
+//         } else {
+//             // Եթե ոչ, ստեղծում ենք նոր զանգված
+//             reversed[value] = [key];
 //         }
 //     }
-// console.log(a + b);
 
-// for( let i =1; i!== 20; i +=2 ){
-//     console.log(i);
+//     return reversed;
 // }
+// console.log(reverseObject({ a: '1', b: '2' }));
+// console.log(reverseObject({ a: '1', b: '2', c: '2' }));
+// console.log(reverseObject({ a: '1', b: '2', c: '2', d: '2' }));
 
 
-// let k = 5;
-// while( k < 15){
-//     k++
-//     console.log(k);
-// }
-
-
-
-// let points = 0;
-// let harc0 = prompt( " what is the capital of Armenia?");
-// if (harc0 === "Yerevan"){
-//    points = points + 1;
-// }
-// let harc1 = prompt(" what is the capital of Russia?");
-// if (harc1 === "Moskow"){
-//     points = points + 1;
-// }
-// let harc2 = prompt(" what is the capital of France?");
-// if (harc2 === "Paris"){
-//     points = points + 1;
-// }
-//     console.log(" you reseved" +" " + points+ " " + "of 3");
-
-
-// function Number(num) {
-//    if ( num <= 1){
-//     return false;
-//    } 
-//    let sum = 0;
-// for ( let i = 1; i <= num / 2; i++){
-//     if ( num % i === 0){
-//         sum +=i;
-//     }
-// }
-// return sum === sum;
-// }
-// const number = 20;
-// if (Number( number)){
-//     console.log("{number} kataryal tiv e");
-// }else {
-//     console.log("{number}kataryal tiv che");
-// }
- 
-// let x = +prompt();
-// let y = +prompt();
-// let z = +prompt();
-// let max= x > y ? x > z ? x :z : y > z ? y :z;
-// console.log(max);
-
-// let n = +prompt();
-// let x = 0;
-// while( n >= x ){
-//     if ( n % x === 0){
-//         console.log(x);
-//     }
-//     x++;
-// }
-
-// let sum1 = 0;
-// let sum2 = 0;
-// for( let i = 1; i < 100; i++){
-//     if(i % 2 == 0){
-//         sum1 = sum2 + 1;
-//     }else {
-//         sum2 = sum2 + 1;
-//     }
-// }
-// console.log(sum1);
-// console.log(sum2);
-
-
-
-
-// let x = +prompt();
-// let a = 0;
-// for (let i = 0; i < 4; i++) { a += x ** i };
-// console.log(a);
-
-// let n = 1;
-// while( n > 0){
-//     console.log(n);
-// }
-
-
-// let n = 1;
-// do {
-//     console.log(n);
-// } while (true) {
-// }
-
-// for ( i = 1; i > 0; i++){
-//     console.log(i);
-// }
-
-
-// let n = +prompt( " number");
-// sum = 0;
-// let a;
-// while(n){
-//     a = n % 10;
-//     n = (n - a)/ 10;
-//     sum +=a;
-// }
-// console.log(sum);
-
-
-
-// let n = +prompt("n");
-// let sum = 0;
-// n = String(n);
-// for( let i = 0; i < n.lenght; i++){
-// sum = sum + Number(n[i]);
-// }
-// console.log(sum);
-
-
-
-
-// let x = 1;
-// let u = ++x + x++ - --x + ++x;
-// console.log(x);
-// console.log(u);
-
-
-// let x = 5;
-// do{
-//     console.log(x++);
-// }while( x > 5 && x < 10);
-
-
-// let p = prompt("enter  number");
-// for( i = 0; i <= p; i++){
-//     for( let j = 1; j <= 9 ; j++){
-//         let rezultat i * j;
-//         console.log(`${i} x ${j}`);
-//     }
-
-
-// let x = +prompt(" number");
-// let a = 0;
-// for ( let i = 10; i <= 99; i++){
-//     if ( x % 3 === 0 && x % 5 === 0){
-//         a = a + 1;
-//     }
-// }
-// console.log(a);
-
-// let sum = 0;
-// for ( let i = 10; i < 100; i++ ){
-// if ( i % 3 === 0 &&  i % 5 === 0){
-//     sum = i + sum;
-// }
-// }
-// console.log(sum);
-
-
-// for ( let i = 10; i < 100; i++){
-// if( i % 3 === 0 && i % 5 === 0){
-//     console.log(i);
-// }
-// }
-
-// function factorial(n) {
-//         if (n === 0 || n ===1){
-//             return 1
-    
-//         }else {
-//             return n *factorial(n - 1)
-//         }
-//     }
-//     console.log(factorial(5));
-
-
-// let num = +prompt();
-// if (num <= 0){
-//     console.log(false);
-// }
-// let sum = 0;
-// for( let i = 1; i <= num / 2; i++){if( num % i ===0){sum +=i;}}{
-//     console.log(sum === num);
-
-
-
-
-// let n = +prompt();
-// let a = 1;
-// for (let i = 1; i <= n; i++) { a *= i };
-// console.log(a);
-
-
-
-// let a = 0;
-// let b = +prompt();
-// for (let i = 1; i < b; i++) {
-//     if (b % i == 0)
-//         a += i;
-// }
-// if (a == b)
-//     console.log(" tiv@ kataryal e");
-// else (console.log(" tiv@ kataryal che"));
-
-
-
-// for (let i = 1; i < 13; i++){
-//     for ( let j = 0; j < 60; j +=5){
-//         console.log(`${i} : ${j}`);
-//     }
-// }
